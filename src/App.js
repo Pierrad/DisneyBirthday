5c368d51abc743f9feb17997642a0f4ebab98d1d
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-11-17 16:20:17 +0100

    :sparkles: Add reservationCard

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import Lottie from 'react-lottie';
 import { isMobile } from "react-device-detect";
 import { Fireworks } from 'fireworks-js/dist/react'
+import hotel from './assets/img/hotel.jpeg';
+import { ReactComponent as StudioLogo } from './assets/img/studio.svg';
+import { ReactComponent as LandLogo } from './assets/img/land.svg';
+
 
 import disneyData from './assets/lottie/disney.json'
+import birthdayData from './assets/lottie/birthday.json'
 
-const defaultOptions = {
+const birthdayOptions = {
+  loop: true,
+  autoplay: true, 
+  animationData: birthdayData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+}
+
+const disneyOptions = {
   loop: true,
   autoplay: true, 
   animationData: disneyData,
@@ -29,7 +43,7 @@ const options = {
   },
   mouse: { 
     click: false, 
-    move: true, 
+    move: false, 
     max: 3 
   }
 }
@@ -53,14 +67,64 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="spinner">
-          <Lottie options={defaultOptions}
-            height={isMobile ? 350 : 600}
-            width={isMobile ? 350 : 600}
-            isStopped={isStopped}
-            isPaused={false}
-          />
-        </div>
+        {!isStopped && (
+          <div className="spinner">
+            <Lottie options={birthdayOptions}
+              height={isMobile ? 350 : 600}
+              width={isMobile ? 350 : 600}
+              isStopped={isStopped}
+              isPaused={false}
+            />
+          </div>
+        )}
+        {isStopped && (
+          <>
+            <div className="spinner2">
+              <Lottie options={disneyOptions}
+                height={isMobile ? 350 : 550}
+                width={isMobile ? 350 : 550}
+                isStopped={isStopped}
+                isPaused={false}
+              />
+            </div>
+            <div className="reservationCard">
+              <div className="left">
+                <div className="ImageContainer">
+                  <img className='img' src={hotel} alt="hotel" />
+                </div>
+              </div>
+              <div className="right">
+                <div className="title">
+                  <h1>Disney's Sequoia Lodge</h1>
+                  <div className="datesContainer">
+                    <div className="dates">
+                      <div className="beginDate">
+                        <p>
+                          dimanche<br />
+                          <strong>19 déc. 2021</strong>
+                        </p>
+                      </div>
+                      <div className="endDate">
+                        <p>
+                          lundi<br />
+                          <strong>20 déc. 2021</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="logos">
+                    <a href="https://www.disneylandparis.com/fr-fr/destinations/parc-disneyland/" target="_blank" rel="noreferrer">
+                      <LandLogo className="logo"/>
+                    </a>
+                    <a href="https://www.disneylandparis.com/fr-fr/destinations/parc-walt-disney-studios/" target="_blank" rel="noreferrer">
+                      <StudioLogo className="logo"/>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </>
+        )}
         {isStopped && <Fireworks options={options} style={style} />}
       </header>
     </div>
