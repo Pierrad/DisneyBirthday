e7818e3f1c029e9123bd543e63df8259e0c6acda
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-11-17 13:23:32 +0100

    :sparkles: Add lottie and fireworks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Lottie from 'react-lottie';
+import { isMobile } from "react-device-detect";
+import { Fireworks } from 'fireworks-js/dist/react'
+
+import disneyData from './assets/lottie/disney.json'
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true, 
+  animationData: disneyData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+}
+
+const options = {
+  speed: 3,
+  particles: 300,
+  sound: {
+    enable: true,
+    files: [
+      'explosion0.mp3',
+      'explosion1.mp3',
+      'explosion2.mp3'
+    ],
+    volume: { min: 1, max: 2 },
+  },
+  mouse: { 
+    click: false, 
+    move: true, 
+    max: 3 
+  }
+}
+
+const style = {
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  position: 'fixed',
+  background: '#FFF'
+}
 
 function App() {
+  const [isStopped, setStopped] = useState(false);
+
+  setTimeout(() => {
+    setStopped(true);
+  }, 4000);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="spinner">
+          <Lottie options={defaultOptions}
+            height={isMobile ? 350 : 600}
+            width={isMobile ? 350 : 600}
+            isStopped={isStopped}
+            isPaused={false}
+          />
+        </div>
+        {isStopped && <Fireworks options={options} style={style} />}
       </header>
     </div>
   );
